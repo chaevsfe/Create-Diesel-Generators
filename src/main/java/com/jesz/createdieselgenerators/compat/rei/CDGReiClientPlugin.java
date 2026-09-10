c16a4fb272484669b3c3f25881db4980de08909e
chaevsfe <pie@duck.com>
2026-09-10 14:12:02 -0400

    Register REI plugins at the recipe viewer plugin priority

--- src/main/java/com/jesz/createdieselgenerators/compat/rei/CDGReiClientPlugin.java
+++ src/main/java/com/jesz/createdieselgenerators/compat/rei/CDGReiClientPlugin.java
@@ -1,9 +1,15 @@
 package com.jesz.createdieselgenerators.compat.rei;
 
+import dev.chaevsfe.createreiviewer.api.CreateReiApi;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 
 public class CDGReiClientPlugin implements REIClientPlugin {
+    @Override
+    public double getPriority() {
+        return CreateReiApi.PLUGIN_PRIORITY;
+    }
+
     @Override
     public void registerCategories(CategoryRegistry registry) {
         if (!CDGReiSupport.available()) {
--- src/main/java/com/jesz/createdieselgenerators/compat/rei/CDGReiCommonPlugin.java
+++ src/main/java/com/jesz/createdieselgenerators/compat/rei/CDGReiCommonPlugin.java
@@ -1,11 +1,17 @@
 package com.jesz.createdieselgenerators.compat.rei;
 
+import dev.chaevsfe.createreiviewer.api.CreateReiApi;
 import me.shedaniel.rei.api.common.plugins.PluginManager;
 import me.shedaniel.rei.api.common.plugins.REICommonPlugin;
 import me.shedaniel.rei.api.common.registry.ReloadStage;
 import me.shedaniel.rei.api.common.registry.display.ServerDisplayRegistry;
 
 public class CDGReiCommonPlugin implements REICommonPlugin {
+    @Override
+    public double getPriority() {
+        return CreateReiApi.PLUGIN_PRIORITY;
+    }
+
     @Override
     public void registerDisplays(ServerDisplayRegistry registry) {
         if (!CDGReiSupport.available()) {
